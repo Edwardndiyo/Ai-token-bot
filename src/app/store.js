a12fcdc98bf8cc5d8333048d0ed2b0dc71c43492
ndiyo <ndiyoedward@gmail.com>
2025-01-18 11:33:45 +0100

    updates

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,19 @@
 import { configureStore  } from "@reduxjs/toolkit";
-// import { userReducer } from "../features/userSlice";
+import userReducer from "../features/userSlice";
 import { userSlice } from "../features/userSlice";
+import messageReducer from "../features/messageSlice";
+import calculateReducer from "../features/calculateSlice";
+import coinShowReducer from "../features/coinShowSlice";
+import topUsersReducer from "../features/topUsersSlice";
 
 
 export const store = configureStore({
     reducer: {
-        user: userSlice,
-    }
-})
+        user: userReducer,
+        message: messageReducer,
+        calculate: calculateReducer,
+        coinShow: coinShowReducer,
+        topUsers: topUsersReducer,
+    },
+});
+
--- src/components/MiningButton.js
+++ src/components/MiningButton.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { selectUser } from "../features/userSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import { 
+  doc,
+  getDoc,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+ } from "firebase/firestore";
+ import { db } from "../firebase";
+ import { selectCalculated } from '../features/calculateSlice';
+ import { setShowMessage } from '../features/messageSlice';
+ import { setCoinShow } from '../features/coinShowSlice';
 
 function MiningButton() {
+  const dispatch = useDispatch();
+  
   return (
     <div>MiningButton</div>
   )
